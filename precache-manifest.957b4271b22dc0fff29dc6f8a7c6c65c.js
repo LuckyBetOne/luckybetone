8b6c46c3ebde367617ee4323ec62f1fb37879b39
Bo Hu <tyranthu@hotmail.com>
2019-01-02 23:10:21 +1100

    init

---
+++ precache-manifest.957b4271b22dc0fff29dc6f8a7c6c65c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5b5bc6d7f5fedc8ef7223933d4df918",
+    "url": "/static/media/dice.f5b5bc6d.svg"
+  },
+  {
+    "revision": "7e85714d729d8d539c6fef0b9e8c06b6",
+    "url": "/static/media/bg.7e85714d.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e3a9b52fd126c1384965",
+    "url": "/static/js/main.e3a9b52f.chunk.js"
+  },
+  {
+    "revision": "f16b64a646db9772ac9e",
+    "url": "/static/js/1.f16b64a6.chunk.js"
+  },
+  {
+    "revision": "e3a9b52fd126c1384965",
+    "url": "/static/css/main.303cfeb5.chunk.css"
+  },
+  {
+    "revision": "9ea8f1cf8b7118ee0360766b144a0235",
+    "url": "/index.html"
+  }
+];
